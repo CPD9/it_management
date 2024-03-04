2da45fc642c760dbe4499885787a9defa90d3eb5
CPD9 <dikepeter9@gmail.com>
2024-03-04 12:42:41 +0100

    first commit

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,43 @@
-export { Header } from "./header";
+// import Accordion from "./accordion";
+
+import UpcomingEvents from "./home/upcoming-events";
+import DealsCharts from "./home/deals-charts";
+
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+
+import DashboardTotalCountCard from "./home/total-count-card";
+import LatestActivities from "./home/latest-activities";
+
+// import ContactStatusTag from "./tags/contact-status-tag";
+
+
+export { 
+    UpcomingEvents,
+    DealsCharts,
+
+    UpcomingEventsSkeleton,
+    ProjectCardSkeleton,
+    KanbanColumnSkeleton,
+    LatestActivitiesSkeleton,
+    AccordionHeaderSkeleton,
+
+    DashboardTotalCountCard,
+    LatestActivities,
+
+    // ContactStatusTag,
+ };
+
+
+ export * from "./tags/user-tags"
+ export * from "./text"
+ export * from "./accordion"
+ export * from "./tasks/form/description"
+ export * from "./tasks/form/due-date"
+ export * from "./tasks/form/header"
+ export * from "./tasks/form/stage"
+ export * from "./tasks/form/title"
+ export * from "./tasks/form/users"
